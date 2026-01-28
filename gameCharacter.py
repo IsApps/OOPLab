bd4d4ecc7f2fb380d321854c549d34e7e520b459
IsApps <96055972+IsApps@users.noreply.github.com>
2026-01-28 10:53:49 -0500

    Update gameCharacter.py

--- gameCharacter.py
+++ gameCharacter.py
@@ -10,3 +10,9 @@ class GameCharacter:
 
     def heal(self, amount):
         self.health += amount
+
+
+character = GameCharacter("Hero", 100)
+character.take_damage(30)
+character.heal(10)
+print(character.health)
